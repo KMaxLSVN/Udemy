118fa4f1cae6bc095d886439dbeffd226b3345de
Max <maxim250291@gmail.com>
2020-05-25 17:55:46 +0300

    Section 19 - http - firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {
     public collapsed: boolean = true;
+
+    constructor(private dataStorageService: DataStorageService) { }
+
+    onSaveData() {
+        this.dataStorageService.storeRecipe();
+    }
+
+    onFetchData() {
+        this.dataStorageService.fetchRecipes().subscribe();
+    }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,34 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Array<Recipe> = [
-        new Recipe(
-            'Tasty Schnitzel',
-            'A super-tasty Schnitzel - just awesome!',
-            'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]
-        ),
-        new Recipe(
-            'Big Fat Burger',
-            'What else you need to say',
-            'https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-760w.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ]
-        )
-    ];
+    // private recipes: Array<Recipe> = [
+    //     new Recipe(
+    //         'Tasty Schnitzel',
+    //         'A super-tasty Schnitzel - just awesome!',
+    //         'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Big Fat Burger',
+    //         'What else you need to say',
+    //         'https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-760w.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ]
+    //     )
+    // ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
+
+    setRecies(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes(): Recipe[] {
         return this.recipes.slice();
@@ -55,7 +61,7 @@ export class RecipeService {
     }
 
     deleteRecipe(index: number) {
-        this.recipes.splice(index, 0);
+        this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
     }
 }
