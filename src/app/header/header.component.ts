919e41ec9dac346dd8e11da528fba2cd8aad6c19
Max <maxim250291@gmail.com>
2020-05-26 21:27:09 +0300

    Section 20 - Token and Interceptor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
-import { DataStorageService } from '../shared/data-storage.service';
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
+export class HeaderComponent implements OnInit, OnDestroy {
+  collapsed = true;
+  isAuthenticated = false;
+    
+  private userSub: Subscription;
 
-export class HeaderComponent {
-    public collapsed: boolean = true;
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
-    constructor(private dataStorageService: DataStorageService) { }
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+        this.isAuthenticated = !!user;
+    });
+  }
 
-    onSaveData() {
-        this.dataStorageService.storeRecipe();
-    }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 
-    onFetchData() {
-        this.dataStorageService.fetchRecipes().subscribe();
-    }
+  onSaveData() {
+    this.dataStorageService.storeRecipe();
+  }
 
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
